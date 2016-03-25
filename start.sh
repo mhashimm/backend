569bb23a04ef9c6372e4ade786cda59859a59bff
mhashim <mhashim.gm@gmail.com>
2016-03-25 10:49:57 +0300

    Use MySQL on different app/gear

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export OPENSHIFT_EXT_MYSQL_DB_HOST=frontend-sisdn.rhcloud.com
+export OPENSHIFT_EXT_MYSQL_DB_PORT=3306
+export OPENSHIFT_EXT_MYSQL_DB_USER=adminYqSuJsi
+export OPENSHIFT_EXT_MYSQL_DB_NAME=frontend
 
 sbt run
